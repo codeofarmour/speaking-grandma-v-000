ac227a69c0957fe40a19e64feeef5b43cf064816
Terri Riggle <codeofarmour@outlook.com>
2017-09-30 05:03:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
+  if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?"
+  elsif phrase = "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!"
-  else phrase == "WHAT DID YOU EAT TODAY?"
+  else phrase = "WHAT DID YOU EAT TODAY?"
     puts "NO, NOT SINCE 1938!"
 end
 end
