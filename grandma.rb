27b53e11edd01ebc0461c16d719e6db96da75817
Terri Riggle <codeofarmour@outlook.com>
2017-09-30 05:31:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,13 +7,13 @@
   else
     return "I LOVE YOU TOO PUMPKIN!"
   end
-=end 
+=end
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif response == response.upcase
-    return  "NO, NOT SINCE 1938!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP, SONNY!"
-end
+    "HUH?! SPEAK UP, SONNY!"
+  end
 end
