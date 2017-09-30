3069fc7c55aad0aab74fdb094b1181b40faf40ac
Terri Riggle <codeofarmour@outlook.com>
2017-09-30 05:31:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,3 @@
-=begin def speak_to_grandma(phrase)
-  if phrase != phrase.upcase()
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif
-    phrase = phrase.upcase || phrase.downcase
-     return "NO, NOT SINCE 1938!"
-  else
-    return "I LOVE YOU TOO PUMPKIN!"
-  end
-=end
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
